fdf7c415f71335d6b34b8a9822884d3e8ad5a073
1ongames <114791859+1ongames@users.noreply.github.com>
2024-11-10 18:32:55 +0900

    나는 마소의 ts를 싫어...해?

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
 /* i hate typescript */
+import { Client, GatewayIntentBits } from 'discord.js';
+const token = "TOKEN_INSERT"
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`해당 계정에 로그인하였습니다: ${client.user?.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === '시론아 이프') {
+    message.channel.send('머랭!');
+  }
+  if (message.content === '시론아 머랭') {
+    message.channel.send('쿠키!');
+  }
+});
+
+// 봇 로그인
+client.login(token);
